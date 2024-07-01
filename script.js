fe8bddd460c46f14319690ee47b41c6a5c485da4
JoepSmit1 <joepsmitjr@gmail.com>
2024-07-01 19:25:29 +0200

    -
    -

--- script.js
+++ script.js
@@ -9,9 +9,10 @@ if(leeftijd >= toegangsLeeftijd){
   console.log("Helaas je bent niet oud genoeg")
 }
 
-// Met een constante wordt bedoeld dat de waarde van het veld niet meer kan veranderens
+// Met een constante wordt bedoeld dat de waarde van het veld niet meer kan veranderen
 // queryselector is een functie die het eerst voorkomende element in het document zoekt op basis van een atrribute (tag, id, class)
 // querySelectorAll selecteert die alle "square" in het document
+
 const squares = document.querySelectorAll('.square')
 const promes = document.querySelector('.promes')
 // # = ID en . = class 
@@ -21,11 +22,11 @@ const score = document.querySelector('#score')
 
 // Het trefwoord let in JavaScript wordt gebruikt om een ​​variabele te declareren die in JavaScript een blokbereik heeft
 // je begint met resultaat 0, dit veranderd later, daarom is het een variabel
-let result = 0 //Nummer
-let hitPosition
-let currentTime = 60 //Nummer
-let timerId = null //Wanneer een timerId null is, dan is die klaar, of moet die nog beginnen en dan start die opnieuw.
 
+let result = 0 //Number
+let hitPosition 
+let currentTime = 60 //Number
+let timerId = null //CC: Chatgpt //Dit had ik nodig om mijn spel te resetten
 
 function play() { //deze functie hebben we niet behandeld in de les en heb ik van CHATGPT
   var audio = new Audio('oei.mp3')
@@ -42,6 +43,8 @@ function randomSquare() {
 // deze functie kies een random vierkant, waar promes in word gezet
 // Met Math.random word er een random vierkant gekozen van de 9.
 // Met Math.floor word het nummer naar beneden afgerond
+// Ik had eerst Math.Ceil maar toen verdween promes soms
+
   let randomSquare = squares[Math.floor(Math.random() * 9)]
 
 // met randomSquare.classlist word Promes in een van de 9 vierkanten gezet
@@ -52,32 +55,30 @@ function randomSquare() {
   hitPosition = randomSquare.id
 }
 
-
-squares.forEach(square => {
-  square.addEventListener('mousedown', () => {
+squares.forEach(square => { 
+  square.addEventListener('mousedown', () => { //CC: Chat GPT // De code die moet worden uitgevoerd wanneer de muisknop wordt ingedrukt op het square element
     if (square.id == hitPosition) { //Dit zorgt ervoor als je op een vierkant klikt waar promes op staat je een punt erbij krijgt en je score omhoog gaat.
       result++
       score.textContent = result
-      hitPosition = null //als je het resultaat hebt gehad begint die weer op 0
+      hitPosition = null 
       audio.play() // hier wordt de audio afgespeeld
     }
   })
 })
 
-// Deze functie zorgt ervoor, dat Promes iedere 500 millie seconde per vierkant word verplaats.
+//CC: SLides op DLO
 function movePromes() {
   timerId = setInterval(randomSquare, 500)
 };
 
-// Geen gebruik gemaakt van parameters
-
-movePromes();
+movePromes();  //Roept de functie aan
 
 function countDown() {
- currentTime-- //met dezer functie loopt de timer af
+ currentTime--
  timeLeft.textContent = currentTime;
 
- //dit zorgt ervoor dat alles gewist wordt.
+ //Met clearInterval kun je een gestarte interval stopzetten. Hiervoor is het nodig dat de interval aan een variabele is toegekend zodat je naar de interval in kwestie kan verwijzen
+ // CC Slides op DLO en CC: https://www.codecademy.com/resources/docs/javascript/window/clearInterval voor een uitleg over Clearinterval
  if (currentTime == 0) {
    clearInterval(countDownTimerId)
    clearInterval(timerId)
@@ -86,11 +87,13 @@ function countDown() {
 
 };
 
-let countDownTimerId = setInterval(countDown, 1000);
+//Met een setInterval wordt een functie herhaaldelijk na het verstrijken van een opgegeven aantal milliseconden uitgevoerd.
+// CC: SLides op DLO
+let countDownTimerId = setInterval(countDown, 1000); 
 
 // Bronnen
 // CC: Powerpoint slides op DLO
 // CC: ChatGPT
-// CC: Youtube Tutorial
-// CC: FreeCodeCamp.org
+// CC: Youtube
+// CC: CodeAcademy
 // CC: Kennisclips van Arthur
